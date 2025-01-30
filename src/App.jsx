027d84509c25c75b4b9a86568d8149a483fc995a
JubayerAhmedSamrat <jasamrat@gmail.com>
2025-01-30 16:02:15 +0600

    adding .env to gitignor

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router"
+import Home from "./components/Home/Home"
+import Main from "./components/Main/Main"
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+import { RouterProvider } from "react-router/dom"
+
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
+        },
+        {
+          path:'/register',
+          element:<Register></Register>
+        }
+      ]
+    }
+  ])
+
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  )
+}
+
+export default App
